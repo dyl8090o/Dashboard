2580d0e2f5e8e0c475ec057875167640de3aa8d8
dyl8090o <dylnvista@gmail.com>
2026-08-05 23:59:05 -0700

    fixed loading on count 2 while showing on count 1

--- scripts/counter.js
+++ scripts/counter.js
@@ -13,7 +13,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 let number = 0
-let activeCount = "count2";
+let activeCount = "count1";
 let counterListener = null;
 
 
