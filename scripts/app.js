fbf66601ad47b5755290a61192d6bf913b33aa27
dyl8090o <dylnvista@gmail.com>
2026-08-20 21:18:04 -0700

    now -schools works

--- scripts/app.js
+++ scripts/app.js
@@ -79,7 +79,7 @@ document.addEventListener('touchend', function (event) {
 
 
 onAuthStateChanged(auth, (user) => {
-  if (user && user.email === "dylnvista@gmail.com" || user && user.email === "2026210@lusd.org"){
+  if (user && user.email === "dylnvista@gmail.com" || user && user.email === "2026210@lusd.org" || user && user.email === "2026210@lompocschools.org"){
     loadAlarms();
     loadCountdowns();
     loadReminders();
